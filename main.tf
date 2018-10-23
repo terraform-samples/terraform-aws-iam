d91679d6e086dde61dff310eb126aa4ff5e21d6f
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-10-23 21:38:51 +0200

    Adding iam account module

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "aws-iam-account" {
+  source = "github.com/terraform-library/aws-iam-account"
+
+  account_alias = "test-account-awesome-compaany"
+
+  minimum_password_length = 6
+  require_numbers         = false
+}
+
+
+
+
---
+++ provider.tf
@@ -0,0 +1,6 @@
+# Specify the provider and access details
+provider "aws" {
+  shared_credentials_file = "./credentials"
+  profile = "default"
+  region = "eu-west-1"
+}
